934855adf2deed1722666970045c176896d9db1a
Ishaan <Kaul373.ik@gmail.com>
2024-09-21 02:23:05 +0530

    add vipes + config loader

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"database/sql"
 	"log"
-	
-    _ "github.com/lib/pq"
+
 	"github.com/ikaul29/Bank/api"
 	db "github.com/ikaul29/Bank/db/sqlc"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/ikaul29/Bank/util"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	
 	//establish connection
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -25,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
     if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
