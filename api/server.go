851ff9e9702d6ed3603c01b8480732e583fa5d49
Ishaan <Kaul373.ik@gmail.com>
2024-09-29 13:26:54 +0530

    updates

--- api/server.go
+++ api/server.go
@@ -1,21 +1,37 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/ikaul29/Bank/db/sqlc"
+	"github.com/ikaul29/Bank/token"
+	"github.com/ikaul29/Bank/util"
 )
 
 // Server serves HTTP requests for our banking service
 type Server struct {
-	store db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -32,7 +48,7 @@ func NewServer(store db.Store) *Server {
 	router.GET("/users", server.getUser)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
